0b7d5a721452203d923ed8e20f6d9555a31cb961
aryamansharma01 <aryamansharma01@gmail.com>
2020-11-28 15:46:45 +0530

    error handling

--- wash_assemble.py
+++ wash_assemble.py
@@ -1,9 +1,9 @@
 import Pass
-    # Defining the hardcodes of the instructions present in our system
-    # MRI-> Memory reference
-    # REGREF -> Register reference
-    # IO -> Input/ output
-    # PSEUDO -> Pseudo instructions
+# Defining the hardcodes of the instructions present in our system
+# MRI-> Memory reference
+# REGREF -> Register reference
+# IO -> Input/ output
+# PSEUDO -> Pseudo instructions
 from hardcode import *
 # print("Memory Reference :",MRI)
 # print()
@@ -24,20 +24,17 @@ if(len(name.split('.')) == 2):
             print(RED.format("Sorry, the file wasn't found. Please check directory for the given file"))
             exit()
         program = file.readlines()
-        #print(program)
+        print(program)
         program = [' '.join(i.split(";", 1)[0].split()) for i in program]
         program = [i.upper() for i in program if i]
-        #print(program)
-
-        # condition 1
-        condition1 = 0
+        print(program)
+        condition = [0, 1, 0, 0, 1, 1]
         if program[0] == 'SRT':
             print("No start error")
-            condition1 = 1
+            condition[0] = 1
         else:
-            print(RED.format("Program is not started by SRT"))
+            print("Program is not started by SRT")
         # condition 2
-        condition2 = 1
         for i in program:
             if(i == 'STP'):
                 break
@@ -46,83 +43,85 @@ if(len(name.split('.')) == 2):
                 if j == " ":
                     c = c+1
             if(c > 2):
-                print(RED.format("Line \"", i, "\" has more than 3 fields"))
-                condition2 = 0
+                print("Line \"", i, "\" has more than 3 fields")
+                condition[1] = 0
                 break
-        if condition2 == 1:
+        if condition[1] == 1:
             print("No syntax error")
 
-        # condition 3
-        condition3 = 0
+            # condition 3
         address = set()
         for i in program:
             if(i == 'STP'):
                 eof = program.index(i)
                 break
-        if(i[0:3] in MRI):
-            address.add(i[4:8].split(' ')[0]) if i[4:8] not in address else address
-            check = set()
-            file2 = open('mandatorySBR.txt', 'r')
-            program2 = file2.readlines()
-            for i in program2:
-                if ',' in i:
-                    check.add(i.split(',')[0]) if i.split(',')[0] not in check else check
-                if ':' in i:
-                    check.add(i.split(':')[0]) if i.split(':')[0] not in check else check
-            for i in program:
-                if ':' in i:
-                    check.add(i.split(':')[0]) if i.split(':')[0] not in check else check
-            #condition3 = 1 if all(i.split(',')[0].split(" ")[0] in address.union(MRI) for i in program) else 0
-            condition3 = 1 if address.difference(check) == set() else 0
-            print("No labelling error") if condition3 == 1 else print(
-                "You missed some labels: ", address.difference(check))
-        # condition 4
-        condition4 = 0
-        labels = []
-        reserved = {'TIM', 'TMP', 'SPS', 'WAT', 'CLN'}
+            if(i[0:3] in MRI):
+                address.add(i[4:8].split(' ')[0]) if i[4:8] not in address else address
+        labels = set()
+        mandatory = set()
+        file2 = open('mandatorySBR.txt', 'r')
+        program2 = file2.readlines()
+        for i in program2:
+            if ',' in i:
+                mandatory.add(i.split(',')[0])
+            if ':' in i:
+                mandatory.add(i.split(':')[0])
         for i in program:
-            if ',' in i and i.split(',')[0].split(" ")[0] not in reserved:
-                labels.append(i.split(',')[0].split(" ")[0])
-            if ':' in i and i.split(':')[0].split(" ")[0] not in reserved:
-                labels.append(i.split(':')[0].split(" ")[0])
-        print(labels)
+            if ':' in i:
+                labels.add(i.split(':')[0]) if i.split(':')[0] not in labels else labels
+            if ',' in i:
+                labels.add(i.split(',')[0]) if i.split(',')[0] not in labels else labels
+        condition[2] = 1 if address.difference(labels) == set(
+        ) and mandatory.difference(address) == set() else 0
+        print("No labelling error") if condition[2] == 1 else print("You missed some labels: ",
+                                                                    (address.difference(labels)).union(mandatory.difference(address)))
+
+        # condition 4
         if(len(labels) != len(set(labels))):
             print("Your labels contain duplicate names ")
         else:
             print("No naming error")
-            condition4 = 1
+            condition[3] = 1
 
         # condition 5 and 6
-        condition5 = 1
-        condition6 = 1
         for i in program:
             if i[0:3] in MRI:
                 if len(i) != 3:
                     continue
                 else:
                     print("MRI error at line containing", i)
-                    condition5 = 0
+                    condition[4] = 0
+                    break
+            elif i[0:3] in REGREF or i[0:3] in ['SKI', 'INP']:
+                if len(i) == 3:
+                    continue
+                else:
+                    print("Non-MRI error at line containing", i)
+                    condition[5] = 0
                     break
             else:
                 if ',' in i or ':' in i:
                     continue
                 else:
-                    if len(i) != 3:
-                        print("Non-MRI error at line containing", i)
-                        condition6 = 0
-                        break
-        if condition5 == 1:
+                    print("Undefined field at line containing", i)
+                    condition[5] = 0
+                    break
+        if condition[4] == 1:
             print("No MRI error")
-        if condition6 == 1:
+        if condition[5] == 1:
             print("No non-MRI error")
         # syntax analysis
         for i in address:
             if i[0].isnumeric() or len(i) > 3 or '.' in i:
                 print(i, "is an invalid address")
-        #Two pass assembly
-        print(GREEN.format("Correct program!\nTranslating..."))
-        Pass.pass1(name)
-        Pass.pass2(name)
+
+        if all(i == 1 for i in condition):
+            # Two pass assembly
+            print(GREEN.format("Correct program!\nTranslating..."))
+            Pass.pass1(name)
+            Pass.pass2(name)
+        else:
+            print("Please rectify the above error(s) before proceeding further")
     else:
         print(RED.format(ext + " extension not suppported.\nPlease enter a file with extensions .txt or .asm"))
 else:
